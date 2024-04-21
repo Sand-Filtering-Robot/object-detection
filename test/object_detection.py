9cc055341db17dd8f41bf3563c48d14fa0c3b09c
kelvin <kelvinandreshau@gmail.com>
2024-04-21 14:13:31 -0400

    added new model

---
+++ test/object_detection.py
@@ -0,0 +1,87 @@
+from picamera2 import Picamera2, Preview
+from PIL import Image
+import torch
+import torchvision
+from torchvision import models 
+from torchvision.transforms import v2
+from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2, FasterRCNN_ResNet50_FPN_V2_Weights
+from imagenet_classes import classes
+from env import verbose
+import time
+import os
+
+# setup picamera
+picam2 = Picamera2()
+preview_config = picam2.create_preview_configuration()
+picam2.configure(preview_config)
+
+# start the preview
+picam2.start_preview(Preview.QTGL)
+
+# start the camera
+picam2.start()
+
+# torch setup
+torch.backends.quantized.engine = 'qnnpack'
+weights = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
+
+# torch image preprocessing
+preprocess = weights.transforms()
+
+# mobile net
+detection_model = fasterrcnn_resnet50_fpn_v2(weights=weights).eval()
+
+# frame counting
+last_logged = time.time()
+last_logged_frame_count = 0
+frame_count = 0
+
+with torch.no_grad():
+    while True:
+        # capture an image
+        if verbose:
+            print('[INFO] capturing image...')
+        raw_image = picam2.capture_image().convert(mode='RGB')
+
+        # pre process the image
+        if verbose:
+            print('[INFO] preprocessing image...')
+        model_input = preprocess(raw_image)
+
+        # adjust to mini-batch size of 1
+        model_input = model_input.unsqueeze(0)
+
+        # run model :)
+        if verbose:
+            print('[INFO] running model...')
+        output = detection_model(model_input)
+
+        #time.sleep(DELAY)
+
+        # print model output
+        predicted_labels_num = output['labels'][:10] # top 10 predicted outputs
+        predicted_scores = output['scores'][:10] # top 10 predicted scores
+        predicted_labels = [weights.meta['categories'][i] for i in predicted_labels_num]
+
+        os.system('clear')
+        for i in range(len(predicted_labels)):
+            print(f'{predicted_labels[i]}: {predicted_scores[i]}')
+        
+        # log frame / performance
+        frame_count += 1
+        now = time.time()
+        print(f"{last_logged_frame_count} fps")
+        if (now - last_logged) > 1:
+            last_logged = now
+            last_logged_frame_count = frame_count
+            frame_count = 0
+        
+
+
+
+
+
+
+
+
+
